eea37ec61204e60c3d349432cc5e15ba415ad4a4
sandman <francisco.vera.95@gmail.com>
2024-07-01 23:15:38 -0600

    creacion de funciones de encripcion

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-function encrptar(){
-    const texto = document.getElementById
+function encriptar(){
+    const texto = document.getElementById("entradaDeTexto").value;
+    const textoEncriptado = texto
+        .replace(/a/g, "ai")
+        .replace( /e/g, "enter")
+        .replace(/i/g, "imes")
+        .replace(/o/g, "ober")
+        .replace(/u/g, "ufat");
+    console.log(textoEncriptado);
+    const salida = document.getElementById("salidaDeTexto")
+    while(salida.firstChild){
+        salida.removeChild(salida.firstChild);
+    }
+    const textoDeSalida = document.createElement("textarea");
+    textoDeSalida.innerText = textoEncriptado;
+    salida.appendChild(textoDeSalida);
+    const boton = document.createElement("button");
+    boton.innerText="copiar";
+    boton.id = "copia"
+    salida.appendChild(boton);
+
 }
