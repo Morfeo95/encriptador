c43c9c6fa8f97bc6ef6f71be14c29905c929c944
Francisco <francisco.vera.95@gmail.com>
2024-07-02 16:01:10 -0600

    creacion de las funciones js

--- app.js
+++ app.js
@@ -1,22 +1,52 @@
 function encriptar(){
     const texto = document.getElementById("entradaDeTexto").value;
     const textoEncriptado = texto
-        .replace(/a/g, "ai")
         .replace( /e/g, "enter")
         .replace(/i/g, "imes")
         .replace(/o/g, "ober")
+        .replace(/a/g, "ai")
         .replace(/u/g, "ufat");
-    console.log(textoEncriptado);
-    const salida = document.getElementById("salidaDeTexto")
+    const salida = document.getElementById("salidaDeTexto");
     while(salida.firstChild){
         salida.removeChild(salida.firstChild);
     }
     const textoDeSalida = document.createElement("textarea");
     textoDeSalida.innerText = textoEncriptado;
+    textoDeSalida.id = "mensajeEncriptado";
     salida.appendChild(textoDeSalida);
     const boton = document.createElement("button");
     boton.innerText="copiar";
-    boton.id = "copia"
+    boton.onclick=copiar();
     salida.appendChild(boton);
+    document.getElementById("entradaDeTexto").value = "Ingrese el texto aqui";
+
+}
+
+function copiar(){
+    const contenidoAside = document.getElementById("mensajeEncriptado").value;
+    navigator.clipboard.writeText(contenidoAside);
+}
+
+function desencriptar(){
+    const texto = document.getElementById("entradaDeTexto").value;
+    const textoEncriptado = texto
+        .replace( /enter/g, "e")
+        .replace(/imes/g, "i")
+        .replace(/ober/g, "o")
+        .replace(/ai/g, "a")
+        .replace(/ufat/g, "u");
+    const salida = document.getElementById("salidaDeTexto");
+    while(salida.firstChild){
+        salida.removeChild(salida.firstChild);
+    }
+    const textoDeSalida = document.createElement("textarea");
+    textoDeSalida.innerText = textoEncriptado;
+    textoDeSalida.id = "mensajeEncriptado";
+    salida.appendChild(textoDeSalida);
+    const boton = document.createElement("button");
+    boton.innerText="copiar";
+    boton.onclick=copiar();
+    salida.appendChild(boton);
+    document.getElementById("entradaDeTexto").value = "Ingrese el texto aqui";
 
 }
